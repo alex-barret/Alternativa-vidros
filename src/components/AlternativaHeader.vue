e9c08b57996214ce195ca085e5643c1ffe208c12
Alex Junior Barreto <alexjuniorbarreto@gmail.com>
2022-10-27 22:20:02 -0300

    header

--- src/components/AlternativaHeader.vue
+++ src/components/AlternativaHeader.vue
@@ -2,7 +2,22 @@
   <header>
     <div class="container nav navbar-expand">
       <a ref="#" class="link navbar-brand mr-auto">
-        <img src="" alt="" />
+        <img
+          src="./../assets/logo-alternativa.jpg"
+          alt="Alternativa vidros"
+          class="img d-inline-block align-top"
+          id="logo"
+        />
+
+        <div class="links mr-2">
+          <a href="#" class="btn mr-5">
+            <i class="fa fa-phone" aria-hidden="true">Belo Horizonte</i>
+          </a>
+          <a href="#" class="btn mr-5">Montes Claros</a>
+          <a href="#" class="btn mr-5">Venda Nova</a>
+          <a href="#" class="btn mr-5">Ipatinga</a>
+          <a href="#" class="btn mr-5">E-mail</a>
+        </div>
       </a>
     </div>
   </header>
