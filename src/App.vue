fbc50bbefa0435773d1ef7a864382dda3a6cab70
Alex Junior Barreto <alexjuniorbarreto@gmail.com>
2022-10-27 22:00:04 -0300

    primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <alternativa-header />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AlternativaHeader from "./components/AlternativaHeader";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    AlternativaHeader,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url("https://fonts.googleapis.com/css?family=Rajdhani&display=swap");
 </style>
---
+++ src/components/AlternativaHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <div class="container nav navbar-expand">
+      <a ref="#" class="link navbar-brand mr-auto">
+        <img src="" alt="" />
+      </a>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
